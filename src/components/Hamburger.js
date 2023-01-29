98feb01bcd10a3d095340eacc9e56c3d805bb567
Jared Staples <jstaplesmusic@gmail.com>
2023-01-29 08:51:27 -0500

    Created useState functionality for dropdown hamburger menu

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,7 +1,28 @@
+import { FaBars } from "react-icons/fa";
+import { useState, useEffect } from "react";
+
+const hamburgerStyle = {
+  position: "absolute",
+  top: 35,
+  right: 35,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+};
+
 export default function Hamburger({ items, handleSectionRendered }) {
+  const [dropdownVisibility, setDropdownVisibility] = useState("hidden");
+
+  const handleDropdown = () => {
+    dropdownVisibility === "hidden"
+      ? setDropdownVisibility("visible")
+      : setDropdownVisibility("hidden");
+  };
+
   return (
-    <nav className="hamburger">
-      <ul>
+    <nav className="hamburger" style={hamburgerStyle}>
+      <FaBars className="FaBars" onClick={handleDropdown} />
+      <ul className="dropdown" style={{ visibility: dropdownVisibility }}>
         {items.map((item) => {
           return (
             <li
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,9 @@
-import { FaBars } from "react-icons/fa";
 import Hamburger from "./Hamburger";
-
-const hamburgerStyle = {
-  position: "absolute",
-  top: 35,
-  right: 35,
-};
 
 export default function Header({ items, handleSectionRendered }) {
   return (
     <div>
       <h1 className="header">Jared Staples</h1>
-      <FaBars className="FaBars" style={hamburgerStyle} />
       <div className="hamburger-div">
         <Hamburger
           items={items}
