ba53d8188d301d6069ceaea2221994d05004521f
Jared Staples <jstaplesmusic@gmail.com>
2023-01-31 17:25:01 -0500

    feat(Project.js): added props object to pass to Project component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,14 +1,31 @@
 import Project from "./Project";
+import taskPic from "../assets/task-management.png";
 import "../styles/Portfolio.css";
+
+const projectObjectTemplate = {
+  name: "",
+  img: "",
+  primaryColor: "",
+  githubUrl: "",
+  deployedSiteUrl: "",
+  shortDescription: "",
+};
+
+const project1 = {
+  name: "Task Manager",
+  img: taskPic,
+  primaryColor: "#ffd755",
+  githubUrl: "https://github.com/cmurphgarv/Project-Management-App",
+  deployedSiteUrl: "https://project-management-2022-but.herokuapp.com/login",
+  shortDescription:
+    "Proident aute esse ad deserunt duis amet minim. Eu ea velit consectetur eu aliqua.",
+};
 
 export default function Portfolio() {
   return (
     <section className="portfolio">
       <h2>Portfolio Component</h2>
-      <Project />
-      <Project />
-      <Project />
-      <Project />
+      <Project projectObj={project1} />
     </section>
   );
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,52 +1,35 @@
-// import taskPic from "../assets/task-management.png";
-// import "../styles/Project.css";
-
-// export default function Project() {
-//   const projects = [
-//     {
-//       name: "Task Management App",
-//       img: "../assets/task-management.png",
-//       url: "https://project-management-2022-but.herokuapp.com/",
-//     },
-//   ];
-//   console.log(projects.img);
-//   return (
-//     <div className="project-container">
-//       <h2>Project Component</h2>
-//       <p>
-//         Reprehenderit sint ea dolore non sunt. Deserunt veniam minim Lorem
-//         proident proident. Reprehenderit commodo id labore eu reprehenderit
-//         exercitation ea proident id aliqua.
-//       </p>
-//       <div className="image-container">
-//         <img src={taskPic} alt="task-manager" />
-//       </div>
-//     </div>
-//   );
-// }
-
 import "../styles/Project.css";
-import taskPic from "../assets/task-management.png";
 
-export default function Project() {
+export default function Project({ projectObj }) {
   return (
     <div className="project-container">
       <div className="left-column">
         <div className="image-container">
-          <img src={taskPic} alt="task-management" />
+          <img src={projectObj.img} alt={projectObj.name} />
         </div>
         <div className="btn-container">
-          <button className="btn github">Github</button>
-          <button className="btn deployed-site">Deployed Site</button>
+          <button
+            className="btn github"
+            style={{ background: projectObj.primaryColor }}
+          >
+            <a href={projectObj.githubUrl} target="_blank">
+              Github
+            </a>
+          </button>
+          <button
+            className="btn deployed-site"
+            style={{ background: projectObj.primaryColor }}
+          >
+            <a href={projectObj.deployedSiteUrl} target="_blank">
+              Deployed Site
+            </a>
+          </button>
         </div>
       </div>
       <div className="right-column">
-        <h2>Project Title Here</h2>
+        <h2>{projectObj.name}</h2>
         <div className="descr-container">
-          <p className="description">
-            short description here short description here short description here
-            short description here short description here short description here
-          </p>
+          <p className="description">{projectObj.shortDescription}</p>
         </div>
       </div>
     </div>
