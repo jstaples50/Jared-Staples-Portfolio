2a0ce1f52eeb2d1927a5591e36af107c6aa129dd
Jared Staples <jstaplesmusic@gmail.com>
2023-01-31 21:11:58 -0500

    style(AboutMe.css) added styling and aesthetics to about me component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,6 @@
 import Project from "./Project";
 import taskPic from "../assets/task-management.png";
+import socialMediaPic from "../assets/social-media-backend.png";
 import "../styles/Portfolio.css";
 
 const projectObjectTemplate = {
@@ -21,11 +22,22 @@ const project1 = {
     "Proident aute esse ad deserunt duis amet minim. Eu ea velit consectetur eu aliqua.",
 };
 
+const project2 = {
+  name: "Social Media Back-End",
+  img: socialMediaPic,
+  primaryColor: "#B55FB1",
+  githubUrl: "https://github.com/jstaples50/social-media-test-back-end",
+  deployedSiteUrl: "https://github.com/jstaples50/social-media-test-back-end",
+  shortDescription:
+    "Officia duis ea aliquip nulla irure esse anim cupidatat fugiat cupidatat ex. Occaecat ut culpa do culpa aliqua veniam enim voluptate et pariatur.",
+};
+
 export default function Portfolio() {
   return (
     <section className="portfolio">
       <h2>Portfolio Component</h2>
       <Project projectObj={project1} />
+      <Project projectObj={project2} />
     </section>
   );
 }
