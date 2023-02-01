a493d83aa6684c0d3fc3d14022976ab00f67ade6
Jared Staples <jstaplesmusic@gmail.com>
2023-01-31 22:06:37 -0500

    feat(Contact.js) added additional input fields and styling to Contact component. Added resume to assets

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,22 @@
+import "../styles/Contact.css";
+
 export default function Contact() {
   return (
     <div className="contact">
       <form>
+        <label for="name">Name</label>
+        <input type="text" placeholder="John Doe" name="name"></input>
         <label for="email">Email</label>
-        <input type="text" placeholder="Enter email here" name="email"></input>
+        <input type="text" placeholder="johndoe@mail.com" name="email"></input>
+        <label id="message-label" for="message">
+          Send me a message!
+        </label>
+        <input
+          id="message-box"
+          type="text"
+          placeholder="Hire Me!"
+          name="message"
+        ></input>
       </form>
     </div>
   );
